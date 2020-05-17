21e7c2727523cd8d704f11212a0e2468df705523
pwndex <pwndex42@gmail.com>
2020-05-17 23:54:39 +0500

    Add frontend backend folders && Add styles scripts and home view

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.sass('resources/sass/frontend.scss', 'public/css')
+	.js('resources/js/frontend.js', 'public/js');
